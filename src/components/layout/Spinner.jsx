bc77730394bead242c49819d54c352a27a059819
jingchengpeng@yahoo.com <jingchengpeng@yahoo.com>
2022-05-21 21:37:00 -0300

    finished section 51

---
+++ src/components/layout/Spinner.jsx
@@ -0,0 +1,10 @@
+import spinner from "./assets/spinner.gif";
+function Spinner() {
+  return (
+    <div className="w-100 mt-20">
+      <img src="spinner"></img>
+    </div>
+  );
+}
+
+export default Spinner;
